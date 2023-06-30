127f984ede11658f52c5a6d2cbcf8d9f3fb76f90
Tsinjoniaina Rakotovao <47777232+mitsinjou@users.noreply.github.com>
2023-06-30 11:09:18 +0300

    animation js

--- script/script.js
+++ script/script.js
@@ -1,28 +1,28 @@
-// Fonction pour afficher les éléments lors du défilement
+// Fonction afficher lors du défilement
 function montrerLorsDefilement() {
-    var elements = document.querySelectorAll(".fade"); // Sélectionne tous les éléments avec la classe "fade"
+    var elements = document.querySelectorAll(".fade"); // Sélectionne tous  "fade"
 
-    // Parcours tous les éléments sélectionnés
+    // Parcours tous les éléments
     elements.forEach(function (element) {
         if (estVisible(element)) {
             // Vérifie si l'élément est visible à l'écran
-            element.classList.add("show"); // Ajoute une classe pour afficher l'élément
+            element.classList.add("show"); // Ajoute une classe pour afficher
         } else {
-            element.classList.remove("show"); // Sinon, retire la classe pour masquer l'élément
+            element.classList.remove("show"); // Sinon, retire la classe pour masquer
         }
     });
 }
 
-// Fonction pour vérifier si un élément est visible à l'écran
+//  vérifier si un élément est visible
 function estVisible(element) {
-    var hauteurFenetre = window.innerHeight; // Récupère la hauteur de la fenêtre du navigateur
-    var positionElement = element.getBoundingClientRect().top; // Récupère la position de l'élément par rapport à la fenêtre
-    var pointApparition = 150; // Définit un point de déclenchement de l'apparition
+    var hauteurFenetre = window.innerHeight; // hauteur de la fenêtre
+    var positionElement = element.getBoundingClientRect().top; //  position de l'élément
+    var pointApparition = 150; // point de déclenchement
 
-    return positionElement < hauteurFenetre - pointApparition; // Retourne vrai si l'élément est suffisamment visible
+    return positionElement < hauteurFenetre - pointApparition; // vrai si l'élément est suffisamment visible
 }
 
-// Gérer l'affichage des éléments lors du défilement
+// affichage des éléments lors du défilement
 window.addEventListener("scroll", montrerLorsDefilement);
 
 // Attendre que le document soit prêt
@@ -30,12 +30,12 @@ $(document).ready(function () {
     // Masquer les éléments avec la classe "box"
     $(".box").hide();
 
-    // Faire apparaître les éléments avec la classe "box" après un délai
+    // apparaître les éléments "box"
     setTimeout(function () {
         $(".box").slideDown(4000); // Faire glisser vers le bas les éléments pendant un temps
     }, 3000); // Attendre 3 secondes avant d'exécuter la fonction
 
-    // Ajouter un effet de zoom sur les titres h1 et h2 lorsqu'on passe la souris dessus
+    // Ajouter un effet de zoom sur les titres h1 et h2
     $("h1, h2").hover(
         function () {
             $(this).css("transform", "scale(1.3)"); // Agrandir le titre
@@ -62,5 +62,18 @@ $(document).ready(function () {
 
     $(".panther-head").mouseleave(function () {
         $(this).animate({ width: "-=10%" }); // Rétrécir l'image de 10%
+    });
+});
+
+// SLIDE
+
+document.addEventListener("DOMContentLoaded", function () {
+    const carousel = document.querySelector(".container-carroussel");
+    const carouselButton = document.querySelector("bouton-carroussel");
+
+    carouselButton.addEventListener("click", function () {
+        const firstItem = carousel.firstElementChild; // premier élément du carrousel
+        carousel.removeChild(firstItem); // Supprime le premier élément du carrousel
+        carousel.appendChild(firstItem); // Ajoute le premier élément à la fin
     });
 });
